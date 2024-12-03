a9bd1bbb719f0e8ab1174b3d07e30fafcd3bdd88
unknown <krzysiaknick@gmail.com>
2024-12-02 19:06:25 -0600

    fixed react page

--- src/app/reactDevTools/page.tsx
+++ src/app/reactDevTools/page.tsx
@@ -1,31 +1,33 @@
+import Image from 'next/image';
+
 export default function react() {
-    return (
-        <div className="container">
-            <h1 className="home-header">React Dev Tools</h1>
-            <div className="containerBlocks">
-            <p className="containerBlocks">The React Developer Tools is a browser extension that helps developers debug and optimize 
-                React applications. It allows you to inspect the React component tree, view and edit props and state, monitor hooks, 
-                and analyze performance through profiling. With features like highlighting updates and inspecting context, it provides 
-                a clear understanding of how your application is structured and performing. It's an essential tool for efficient React 
-                development and troubleshooting, available for Chrome, Firefox, and other Chromium-based browsers. </p>
+  return (
+    <div className="container">
+      <h1 className="home-header">React Dev Tools</h1>
+      <div className="containerBlocks">
+        <p className="containerBlocks">
+          The React Developer Tools is a browser extension that helps developers debug and optimize
+          React applications. It allows you to inspect the React component tree, view and edit props
+          and state, monitor hooks, and analyze performance through profiling. With features like
+          highlighting updates and inspecting context, it provides a clear understanding of how your
+          application is structured and performing. It&apos;s an essential tool for efficient React
+          development and troubleshooting, available for Chrome, Firefox, and other Chromium-based
+          browsers.
+        </p>
 
-                <h1 className="containerBlocks">React Dev Tools can be installed from the chrome web store</h1>
+        <h1 className="containerBlocks">React Dev Tools can be installed from the Chrome Web Store</h1>
 
-            <img src="/images/chromestore.png" alt="chromestore" />
-            <br />
-            <p className="containerBlocks">After installing you will see two new tabs avaiable in your chrome debugger. One called 
-                "Components" and one called "Profiler".
-            </p>
-    
-            <img src="/images/devtool.png" alt="components" />
+        <Image src="/images/chromestore.png" alt="chromestore" width={1200} height={300} />
+        <br />
+        <p className="containerBlocks">
+          After installing you will see two new tabs available in your Chrome debugger. One called
+          &quot;Components&quot; and one called &quot;Profiler&quot;.
+        </p>
 
-            
-
-
-
-
-            </div>
-        </div>
-        
-    )
+        <Image src="/images/devtool.png" alt="components" width={1200} height={300} />
+      </div>
+    </div>
+  );
 }
+
+        
